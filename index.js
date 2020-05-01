cf90cbc86ca051cdab304702baff1f37a85940bc
nhcminh303 <nhcminh303@gmail.com>
2020-05-01 17:35:16 +0700

    query parameters

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app = express();
 
 const port = 3000;
 
+// config
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// data
+var users = [
+  { id: 1, name: "Minh" },
+  { id: 2, name: "Tuan" },
+  { id: 3, name: "Phu" }
+];
+
+// routes
 app.get('/', function(req, res) {
   res.render('index', {
     message: 'Nodejs - Express'
@@ -14,11 +23,18 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
   res.render('users/index', {
-    users: [
-      { id: 1, name: "Minh" },
-      { id: 2, name: "Tuan" },
-      { id: 3, name: "Phu" }
-    ]
+    users: users
+  });
+});
+
+app.get('/users/search', function(req, res) {
+  var searchName = req.query.name;
+  var matchedUsers = users.filter(function(user) {
+    return user.name.toLowerCase().indexOf(searchName.toLowerCase()) !== -1;
+  });
+
+  res.render('users/index', {
+    users: matchedUsers
   });
 });
 
