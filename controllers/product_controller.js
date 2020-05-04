e610a0e97b6f2c599ccbcd9c294275c080d976c8
nhcminh303 <nhcminh303@gmail.com>
2020-05-04 13:59:07 +0700

    pagination

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,7 +1,18 @@
 const db = require('../db');
 
 module.exports.index = function(req, res) {
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
+
+  var getProducts = db.get('products').value();
+  var numberOfPages = getProducts.length / perPage;
+
   res.render('products/index', {
-    products: db.get('products').value()
+    products: getProducts.slice(start, end),
+    numberOfPages: numberOfPages,
+    page: page
   });
 };
